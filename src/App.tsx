9c5757f074308d74b98f629952f777ab56cac2e3
FFiillaattoovv <admirator2015@yandex.ru>
2020-05-09 09:10:38 -0700

    Add code in components

--- src/App.tsx
+++ src/App.tsx
@@ -2,58 +2,62 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-      <div className="App">
-        <div className="todoList">
-          <div className="todoList-header">
-            <h3 className="todoList-header__title">What to Learn</h3>
-            <div className="todoList-newTaskForm">
-              <input type="text" placeholder="title"/>
-              <button>Add</button>
+    return (
+        <div className="App">
+            <div className="todoList">
+                <Header />
+                <List />
+                <Footer />
             </div>
-          </div>
-          <div className="todoList-tasks">
-            <div className="todoList-task">
-              <input type="checkbox" checked={true}/>
-              <span>CSS</span>
-            </div>
-            <div className="todoList-task">
-              <input type="checkbox" checked={false}/>
-              <span>JS</span>
-            </div>
-            <div className="todoList-task">
-              <input type="checkbox" checked={false}/>
-              <span>ReactJS</span>
-            </div>
-            <div className="todoList-task">
-              <input type="checkbox" checked={true}/>
-              <span>Patterns</span>
-            </div>
-          </div>
-          <div className="todoList-footer">
-            <button>All</button>
-            <button>Completed</button>
-            <button>Active</button>
-          </div>
         </div>
-      </div>
-  );
+    );
 }
 
 function Header() {
-  return (
-      <div></div>
-  )
+    return (
+        <div className="todoList-header">
+            <h3 className="todoList-header__title">What to Learn</h3>
+            <div className="todoList-newTaskForm">
+                <input type="text" placeholder="title"/>
+                <button>Add</button>
+            </div>
+        </div>
+    )
 
-}function List() {
-  return (
-      <div></div>
-  )
+}
 
-}function Footer() {
-  return (
-      <div></div>
-  )
+function List() {
+    return (
+        <div className="todoList-tasks">
+            <div className="todoList-task">
+                <input type="checkbox" checked={true}/>
+                <span>CSS</span>
+            </div>
+            <div className="todoList-task">
+                <input type="checkbox" checked={false}/>
+                <span>JS</span>
+            </div>
+            <div className="todoList-task">
+                <input type="checkbox" checked={false}/>
+                <span>ReactJS</span>
+            </div>
+            <div className="todoList-task">
+                <input type="checkbox" checked={true}/>
+                <span>Patterns</span>
+            </div>
+        </div>
+    )
+
+}
+
+function Footer() {
+    return (
+        <div className="todoList-footer">
+            <button>All</button>
+            <button>Completed</button>
+            <button>Active</button>
+        </div>
+    )
 }
 
 export default App;
