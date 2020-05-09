50c5f10ad589b3d2091d6b521c5f349ff7b14776
FFiillaattoovv <admirator2015@yandex.ru>
2020-05-09 09:20:18 -0700

    Take components out of App and put in new folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './App.css';
+import {Header} from "./Header";
+import {List} from "./List";
+import {Footer} from "./Footer";
 
 function App() {
     return (
@@ -11,53 +14,6 @@ function App() {
             </div>
         </div>
     );
-}
-
-function Header() {
-    return (
-        <div className="todoList-header">
-            <h3 className="todoList-header__title">What to Learn</h3>
-            <div className="todoList-newTaskForm">
-                <input type="text" placeholder="title"/>
-                <button>Add</button>
-            </div>
-        </div>
-    )
-
-}
-
-function List() {
-    return (
-        <div className="todoList-tasks">
-            <div className="todoList-task">
-                <input type="checkbox" checked={true}/>
-                <span>CSS</span>
-            </div>
-            <div className="todoList-task">
-                <input type="checkbox" checked={false}/>
-                <span>JS</span>
-            </div>
-            <div className="todoList-task">
-                <input type="checkbox" checked={false}/>
-                <span>ReactJS</span>
-            </div>
-            <div className="todoList-task">
-                <input type="checkbox" checked={true}/>
-                <span>Patterns</span>
-            </div>
-        </div>
-    )
-
-}
-
-function Footer() {
-    return (
-        <div className="todoList-footer">
-            <button>All</button>
-            <button>Completed</button>
-            <button>Active</button>
-        </div>
-    )
 }
 
 export default App;
---
+++ src/List.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export function List() {
+    return (
+        <div className="todoList-tasks">
+            <div className="todoList-task">
+                <input type="checkbox" checked={true}/>
+                <span>CSS</span>
+            </div>
+            <div className="todoList-task">
+                <input type="checkbox" checked={false}/>
+                <span>JS</span>
+            </div>
+            <div className="todoList-task">
+                <input type="checkbox" checked={false}/>
+                <span>ReactJS</span>
+            </div>
+            <div className="todoList-task">
+                <input type="checkbox" checked={true}/>
+                <span>Patterns</span>
+            </div>
+        </div>
+    )
+
+}
