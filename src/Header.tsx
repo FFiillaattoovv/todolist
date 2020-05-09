9230f1ef21c72b001cb6352046a00c3d34550bba
FFiillaattoovv <admirator2015@yandex.ru>
2020-05-09 12:42:44 -0700

    Add components Button and Input

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,12 +1,15 @@
 import React from "react";
+import Input from "./Input";
+import Button from "./Button";
+
 
 export function Header() {
     return (
         <div className="todoList-header">
             <h3 className="todoList-header__title">What to Learn</h3>
             <div className="todoList-newTaskForm">
-                <input type="text" placeholder="title"/>
-                <button>Add</button>
+                <Input/>
+                <Button/>
             </div>
         </div>
     )
