9ef1b290b74b8a52ebf5fa94e99441bb293dbe92
FFiillaattoovv <admirator2015@yandex.ru>
2020-05-10 12:27:44 -0700

    Add props type for button

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -3,11 +3,23 @@ import styles from "./Button.module.css"
 
 type PropsType = {
     text: string
+    type?: "default" | "success" | "danger" | "info"
 }
 
 function Button(props: PropsType) {
+    let css = "";
+
+    if (props.type === "danger") {
+        css = styles.danger
+    }
+    if (props.type === "success") {
+        css = styles.success
+    }
+    if (props.type === "info") {
+        css = styles.info
+    }
     return (
-        <input className={styles.button} type="button" value={props.text}/>
+        <input className={styles.button + " " + css} type="button" value={props.text}/>
     )
 }
 
