c7818a42864acfc2e15479cd4639b8d6742ca91a
FFiillaattoovv <admirator2015@yandex.ru>
2020-05-10 02:54:25 -0700

    Add an array of tasks objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import './App.css';
 import {Todolist} from "./Todolist";
+import {v1} from "uuid";
+
+export type TaskType = {
+    id: string
+    title: string
+    isDone: boolean
+}
 
 function App() {
+    const tasks: Array<TaskType> = [
+        { id: v1(), title: 'CSS', isDone: true},
+        { id: v1(), title: 'JS', isDone: true},
+        { id: v1(), title: 'React', isDone: false}
+    ]
     return (
         <div className="App">
             <Todolist />
