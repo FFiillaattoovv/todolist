bcdf27e863239a75133366e1a694c2e232e21dd4
FFiillaattoovv <admirator2015@yandex.ru>
2020-06-20 07:24:57 -0700

    Add data tasks1 in App

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,16 @@ import './App.css';
 import {Todolist} from "./Todolist";
 
 function App() {
+    let tasks1 = [
+        {id: 1, title: "HTML&CSS", isDone: true},
+        {id: 2, title: "JavaScript", isDone: true},
+        {id: 3, title: "React", isDone: true}
+    ]
     return (
         <div className="App">
             <Todolist title="What to learn"/>
+            <Todolist title="Movies"/>
+            <Todolist title="Songs"/>
         </div>
     )
 }
