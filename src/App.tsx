74da0a2c2237da496c31cd8b62a07d499c9b26f0
FFiillaattoovv <admirator2015@yandex.ru>
2020-06-20 09:49:22 -0700

    Add useState() in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from "./Todolist";
 
@@ -9,14 +9,19 @@ export type TaskType = {
 }
 
 function App() {
-    let tasks: Array<TaskType> = [
+    let initTasks: Array<TaskType> = [
         {id: 1, title: "HTML&CSS", isDone: true},
         {id: 2, title: "JavaScript", isDone: true},
         {id: 3, title: "React", isDone: false}
     ]
 
+    let arr = useState(initTasks);
+    let tasks = arr[0];
+    let setTasks = arr[1];
+
     function removeTask(id: number) {
-        tasks = tasks.filter(t => t.id !== id)
+        let filteredTasks = tasks.filter(t => t.id !== id);
+        setTasks(filteredTasks)
     }
 
     return (
