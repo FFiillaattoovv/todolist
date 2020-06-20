2e9e8900c98791b32bfda43917d11ec4d3fb9bac
FFiillaattoovv <admirator2015@yandex.ru>
2020-06-20 06:07:00 -0700

    Add code in Todolist

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,26 @@ import React from 'react';
 function App() {
     return (
         <div className="App">
-            <Todolist />
+            <Todolist/>
         </div>
     )
 }
 
 function Todolist() {
-return (
-    <div>
-
-    </div>
-)
+    return (
+        <div>
+            <h3>What to learn</h3>
+            <div>
+                <input/>
+                <button></button>
+                <ul>
+                    <li><input type="checkbox" checked={true}/><span>HTML&CSS</span></li>
+                    <li><input type="checkbox" checked={true}/><span>JavaScript</span></li>
+                    <li><input type="checkbox" checked={false}/><span>React</span></li>
+                </ul>
+            </div>
+        </div>
+    )
 }
 
 export default App;
