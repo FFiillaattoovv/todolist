502a8cab63e53094673f3b74104e4f82edcb7b4a
FFiillaattoovv <admirator2015@yandex.ru>
2020-06-24 03:48:30 -0700

    Add onChange in input

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -15,7 +15,7 @@ export function Todolist(props: PropsType) {
         <div>
             <h3>{props.title}</h3>
             <div>
-                <input value={newTaskTitle}/>
+                <input value={newTaskTitle} onChange={() => { setNewTaskTitle("Hello")}}/>
                 <button onClick={() => {props.addTask()}}>+</button>
                 <ul>
                     {
