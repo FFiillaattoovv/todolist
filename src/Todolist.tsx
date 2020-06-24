1fec1868e70002e6695d395dabc0aafb451662e1
FFiillaattoovv <admirator2015@yandex.ru>
2020-06-24 04:50:09 -0700

    Add onKeyPress for input

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -15,26 +15,46 @@ export function Todolist(props: PropsType) {
         <div>
             <h3>{props.title}</h3>
             <div>
-                <input value={newTaskTitle} onChange={(e) => {
-                    setNewTaskTitle(e.currentTarget.value)}
-                }/>
+                <input value={newTaskTitle}
+                       onChange={(e) => {
+                           setNewTaskTitle(e.currentTarget.value)
+                       }}
+                       onKeyPress={(e) => {
+                           if (e.charCode === 13) {
+                               props.addTask(newTaskTitle);
+                               setNewTaskTitle("");
+                           }
+                               }}/>
                 <button onClick={() => {
                     props.addTask(newTaskTitle);
                     setNewTaskTitle("");
-                }}>+</button>
+                }}>+
+                </button>
                 <ul>
                     {
-                        props.tasks.map( t => <li>
+                        props.tasks.map(t => <li>
                             <input type="checkbox" checked={t.isDone}/>
                             <span>{t.title}</span>
-                            <button onClick={ () => {props.removeTask(t.id)}}>+</button>
+                            <button onClick={() => {
+                                props.removeTask(t.id)
+                            }}>+
+                            </button>
                         </li>)
                     }
                 </ul>
                 <div>
-                    <button onClick={ () => {props.changeFilter("all")}}>All</button>
-                    <button onClick={ () => {props.changeFilter("active")}}>Active</button>
-                    <button onClick={ () => {props.changeFilter("completed")}}>Completed</button>
+                    <button onClick={() => {
+                        props.changeFilter("all")
+                    }}>All
+                    </button>
+                    <button onClick={() => {
+                        props.changeFilter("active")
+                    }}>Active
+                    </button>
+                    <button onClick={() => {
+                        props.changeFilter("completed")
+                    }}>Completed
+                    </button>
                 </div>
             </div>
         </div>
