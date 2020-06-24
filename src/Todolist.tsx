f0fe02a9523a97bf25ccf4f8c80128722ef445a6
FFiillaattoovv <admirator2015@yandex.ru>
2020-06-24 04:05:15 -0700

    Add clear input

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -18,7 +18,10 @@ export function Todolist(props: PropsType) {
                 <input value={newTaskTitle} onChange={(e) => {
                     setNewTaskTitle(e.currentTarget.value)}
                 }/>
-                <button onClick={() => {props.addTask(newTaskTitle)}}>+</button>
+                <button onClick={() => {
+                    props.addTask(newTaskTitle);
+                    setNewTaskTitle("");
+                }}>+</button>
                 <ul>
                     {
                         props.tasks.map( t => <li>
