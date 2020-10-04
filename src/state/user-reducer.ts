09c5eef20d66a33ad8697a27ce45816116e40670
FFiillaattoovv <admirator2015@yandex.ru>
2020-10-04 06:03:15 -0700

    Delete App.test

--- src/state/user-reducer.ts
+++ src/state/user-reducer.ts
@@ -10,5 +10,14 @@ type ActionType = {
 }
 
 export const userReducer = (state: StateType, action: ActionType) => {
-
+    switch (action.type) {
+        case "INCREMENT-AGE":
+            state.age = state.age + 1;
+            return state;
+        case "INCREMENT-CHILDREN-COUNT":
+            state.childrenCount = state.childrenCount + 1;
+            return state;
+        default:
+            throw new Error("I don't understand this action type")
+    }
 }
