6aa7c621b0824d1b39d51d8190ff420ebdb639ba
FFiillaattoovv <admirator2015@yandex.ru>
2020-10-04 10:03:18 -0700

    Adds test on change todolist title in todolists-reducer.ts

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -9,15 +9,22 @@ type ActionType = {
 export const todolistsReducer = (state: Array<TodolistType>, action: ActionType): Array<TodolistType> => {
     switch (action.type) {
         case "REMOVE-TODOLIST": {
-            return state.filter(tl => tl.id != action.id);
-        };
+            return state.filter(tl => tl.id !== action.id);
+        }
         case "ADD-TODOLIST": {
             return [...state, {
                 title: action.title,
                 id: v1(),
                 filter: "all"
             }];
-        };
+        }
+        case "CHANGE-TODOLIST-TITLE": {
+            const todolist = state.find(tl => tl.id === action.id);
+            if (todolist) {
+                todolist.title = action.title;
+            }
+            return [...state]
+        }
         default:
             throw new Error("I don't understand this action type")
     }
