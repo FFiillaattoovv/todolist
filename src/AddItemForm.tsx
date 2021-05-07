c5626621be7d27a07ed91f6a6ad4e4d4bbc57096
FFiillaattoovv <admirator2015@yandex.ru>
2021-05-07 14:51:22 +0300

    Fixes a bug (additional rendering) in onKeyPressHandler

--- src/AddItemForm.tsx
+++ src/AddItemForm.tsx
@@ -1,48 +1,50 @@
-import React, {ChangeEvent, KeyboardEvent, useState} from "react";
-import {IconButton, TextField} from "@material-ui/core";
-import {ControlPoint} from "@material-ui/icons";
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+import {IconButton, TextField} from '@material-ui/core';
+import {ControlPoint} from '@material-ui/icons';
 
 type AddItemFormPropsType = {
     addItem: (title: string) => void
 }
 
 export function AddItemForm(props: AddItemFormPropsType) {
-    const [newTaskTitle, setNewTaskTitle] = useState("");
+    const [newTaskTitle, setNewTaskTitle] = useState('');
     const [error, setError] = useState<string | null>(null);
     const onNewTitleChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
         setNewTaskTitle(e.currentTarget.value)
     }
     const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
-        setError(null);
+        if (error !== null) {
+            setError(null);
+        }
         if (e.charCode === 13) {
-            if (newTaskTitle.trim() === "") {
-                setError("Title is required")
+            if (newTaskTitle.trim() === '') {
+                setError('Title is required')
                 return;
             }
             props.addItem(newTaskTitle.trim());
-            setNewTaskTitle("");
+            setNewTaskTitle('');
         }
     }
     const addTask = () => {
-        if (newTaskTitle.trim() === "") {
-            setError("Title is required")
+        if (newTaskTitle.trim() === '') {
+            setError('Title is required')
             return;
         }
         props.addItem(newTaskTitle.trim());
-        setNewTaskTitle("");
+        setNewTaskTitle('');
     }
 
     return (
         <div>
             <TextField value={newTaskTitle}
-                       variant={"outlined"}
-                       label={"Type value"}
+                       variant={'outlined'}
+                       label={'Type value'}
                        error={!!error}
                        helperText={error}
-                   onChange={onNewTitleChangeHandler}
-                   onKeyPress={onKeyPressHandler}
+                       onChange={onNewTitleChangeHandler}
+                       onKeyPress={onKeyPressHandler}
             />
-            <IconButton onClick={addTask} color={"primary"}>
+            <IconButton onClick={addTask} color={'primary'}>
                 <ControlPoint/>
             </IconButton>
         </div>
