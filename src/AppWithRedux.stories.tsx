79e8e8fdcbc1b65e0d427ec69fc06e155e71124b
FFiillaattoovv <admirator2015@yandex.ru>
2021-05-12 13:06:45 +0300

    Connects a decorator to AppWithRedux

--- src/AppWithRedux.stories.tsx
+++ src/AppWithRedux.stories.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import {Meta, Story} from '@storybook/react';
 import AppWithRedux from './AppWithRedux';
+import {ReduxStoreProviderDecorator} from './stories/decorators/ReduxStoreProviderDecorator';
 
 export default {
     title: 'Todolist/AppWithRedux',
     component: AppWithRedux,
-    argTypes: {}
+    decorators: [ReduxStoreProviderDecorator]
 } as Meta;
 
-const Template: Story = (args) => <AppWithRedux {...args} />;
+const Template: Story = () => <AppWithRedux />;
 
 export const AppWithReduxExample = Template.bind({});
-AppWithReduxExample.args = {};
