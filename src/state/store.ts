8cee60200e0ab1320eb2f6c4613cb7856eefca41
FFiillaattoovv <admirator2015@yandex.ru>
2021-08-10 20:29:34 +0300

    Rename thunk to thunkMiddleware

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,14 +1,14 @@
 import {applyMiddleware, combineReducers, createStore} from 'redux';
 import {tasksReducer} from './tasks-reducer';
 import {todolistsReducer} from './todolists-reducer';
-import thunk from 'redux-thunk';
+import thunkMiddleware from 'redux-thunk';
 
 const rootReducer = combineReducers({
     tasks: tasksReducer,
     todolists: todolistsReducer
 });
 
-export  const store = createStore(rootReducer, applyMiddleware(thunk));
+export  const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
 export type AppRootStateType = ReturnType<typeof rootReducer>;
 
