64cd796d88c5d6469b7d30971bb134f689b6343f
FFiillaattoovv <admirator2015@yandex.ru>
2021-08-18 14:29:23 +0300

    Change logics for opening Snackbar

--- src/components/ErrorSnackbar/ErrorSnackbar.tsx
+++ src/components/ErrorSnackbar/ErrorSnackbar.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert, {AlertProps} from '@material-ui/lab/Alert';
+import {useSelector} from 'react-redux';
+import {AppRootStateType} from '../../state/store';
 
 function Alert(props: AlertProps) {
     return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
 
 export function ErrorSnackbar() {
-    const [open, setOpen] = React.useState(false);
+    const error = useSelector<AppRootStateType, string | null>(state => state.app.error);
+    const isOpen = error !== null;
 
     const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
         if (reason === 'clickaway') {
             return;
         }
-
-        setOpen(false);
     };
 
     return (
-        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Snackbar open={isOpen} onClose={handleClose}>
             <Alert onClose={handleClose} severity="error">
-                This is a success message!
+                {error}
             </Alert>
         </Snackbar>
     );
