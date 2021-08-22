efbcb64c57d75e01b619ee0d8e78ce0e2e7666a8
FFiillaattoovv <admirator2015@yandex.ru>
2021-08-22 16:42:46 +0300

    Install react-router-dom and formik

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -4,7 +4,7 @@ const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.1/',
     withCredentials: true,
     headers: {
-        'API-KEY': '8f2534e2-22a4-4052-894e-a66c04807482'
+        'API-KEY': '015f6ef9-f2e2-4c2d-a216-55334fed10bf'
     }
 })
 
